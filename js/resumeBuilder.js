13baf3ebff425915db45b291b26d32d29956bf05
Jose Mariz Melo Formiga Viana <mm@xchema.com>
2015-07-29 11:29:56 -0700

    feat: list of contacts
    Including list of contact on header and footer using Generic contact variable
    from helper.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,3 +3,10 @@ This is empty on purpose! Your code to build the resume will go here.
  */
  // HEADER
  $("#header").prepend(HTMLheaderName.replace('%data%', bio.name)+" "+HTMLheaderRole.replace('%data%', 'Web Developer'));
+
+ // go over contact and append to #topContacts and #footerContacts element on page
+  for(var contact in bio.contacts){
+    //using contact generic instead of static list
+    $("#topContacts").append(HTMLcontactGeneric.replace('%data%', bio.contacts[contact]).replace('%contact%', contact));
+    $("#footerContacts").append(HTMLcontactGeneric.replace("%data%", bio.contacts[contact]).replace("%contact%", contact));
+  }
