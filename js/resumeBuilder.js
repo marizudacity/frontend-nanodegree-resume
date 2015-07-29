8f2946e58ea6f76c640d97fb020e67cb2310fc2e
Jose Mariz Melo Formiga Viana <mm@xchema.com>
2015-07-29 11:22:17 -0700

    feat: Inluding JSON files (test on header title) I have included the JSON files and also run a test retrieving the header initial info from bio.json

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,5 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ // HEADER
+ $("#header").prepend(HTMLheaderName.replace('%data%', bio.name)+" "+HTMLheaderRole.replace('%data%', 'Web Developer'));
